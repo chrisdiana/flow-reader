4ce73404dbe787a306fb8b230579ea41087d7b2f
Chris Diana <cdiana.media@gmail.com>
2024-12-27 01:05:56 -0500

    Update sentry.client.config.js

--- apps/reader/sentry.client.config.js
+++ apps/reader/sentry.client.config.js
@@ -1,7 +1,7 @@
 // This file configures the initialization of Sentry on the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
+/*
 import * as Sentry from '@sentry/nextjs'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
@@ -17,3 +17,4 @@ Sentry.init({
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
 })
+*/
